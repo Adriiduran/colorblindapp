03fbeb966416f27d50049b67f3aa4552a9e92878
admist <adriiiduraan@gmail.com>
2026-07-12 18:43:50 +0200

    Initial commit: Proyecto base de Xcode

---
+++ colorblindapp/colorblindappApp.swift
@@ -0,0 +1,17 @@
+//
+//  colorblindappApp.swift
+//  colorblindapp
+//
+//  Created by admist on 12/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct colorblindappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
